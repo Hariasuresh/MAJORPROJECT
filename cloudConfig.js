ad7a2f960273d0bbabb9f24ae9c92a6b979b76af
Hariasuresh <Hariasuresh1@gmail.com>
2024-03-31 21:33:31 +0530

    fixing error

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,11 +1,21 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-cloudinary.config({
-    cloud_name:process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET,
-});
+// cloudinary.config({
+//     cloud_name:process.env.CLOUD_NAME,
+//     api_key: process.env.CLOUD_API_KEY,
+//     api_secret: process.env.CLOUD_API_SECRET,
+// });
+try {
+  cloudinary.config({
+      cloud_name: process.env.CLOUD_NAME,
+      api_key: process.env.CLOUD_API_KEY,
+      api_secret: process.env.CLOUD_API_SECRET,
+  });
+} catch (error) {
+  console.error('Error configuring Cloudinary:', error);
+}
+
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
